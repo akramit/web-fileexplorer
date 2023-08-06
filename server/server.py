430834f92f32042f7b1a485b89e5444f325e61e0
Amit Roy <amit.akr.roy@gmail.com>
2023-08-06 13:36:43 +0530

    Server update

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
+from doctest import debug
 from flask import Flask, request, jsonify
-import services
+import os
 
 
 app = Flask(__name__)
@@ -10,7 +11,37 @@ def hello_world():
 
 @app.route("/directory",methods=['GET'])
 def get_directory():
-      pass
+      directory_list = os.listdir('.')
+      return jsonify(directory_list)
+
+@app.route('/newdir',methods=['POST'])
+def create_directory():
+    data=request.json
+    directory_name = data.get('directory_name')
+    if not directory_name :
+        return jsonify({"error": "Directory name is required"}), 400
+    try:
+        os.mkdir(directory_name)
+        return jsonify({"message": f"Directory '{directory_name}' created successfully"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route('/newfile',methods=['POST'])   
+def create_file():
+    data = request.json
+    file_name = data.get('file_name')
+    path = data.get('path')
+
+    if not file_name:
+        return jsonify({"error": "File name is required"}), 400
+
+    try:
+        with open(os.path.join(path,file_name), 'w') as f:
+            pass
+        return jsonify({"message": f"File '{file_name}' created successfully"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+        
     
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',port=5432)
+	app.run(debug=True,host='0.0.0.0',port=5432)
